6cf5c1ee9ec9676c259ce255a85cf663f7daf7ec
Divyesh patel <76897577+Divyesh-123@users.noreply.github.com>
2022-04-04 07:45:52 +0530

    update in port

--- index.py
+++ index.py
@@ -10,4 +10,4 @@ def greet():
     return render_template("greet.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=9595, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
